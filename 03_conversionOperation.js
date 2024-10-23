8a3d0d2bc7926673a3ad000f476fa25d9e43b39f
shashwat46 <shashwat.singh0406@gmail.com>
2024-10-24 03:23:11 +0530

    Learn more about type conversion

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "33"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33" => 33
 //"33abc" => NaN
@@ -16,7 +16,7 @@ console.log(valueInNumber)
 let isLoggedIn = "Shashwat"
 
 booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1 => true
 // 0=> false
@@ -26,6 +26,15 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+
+
+//*************Operations******************
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+//https://tc39.es/ecma262/#sec-abstract-operations
